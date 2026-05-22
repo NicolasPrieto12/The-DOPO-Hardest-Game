7244c84139e52867ae97eaad55a6c19839b32c53
NicolasPrieto12 <nicolas@dopo.com>
2026-05-21 22:16:54 -0500

    docs: mejorar Javadoc de MachineProfile con descripcion de cada perfil

--- src/main/java/domain/MachineProfile.java
+++ src/main/java/domain/MachineProfile.java
@@ -3,6 +3,14 @@ package domain;
 /**
  * Define los perfiles de comportamiento disponibles para la máquina
  * en el modo Player vs Machine.
+ *
+ * <p>Perfiles disponibles:</p>
+ * <ul>
+ *   <li>{@link #RANDOM}: cambia de dirección cada 25 ticks, 65% probabilidad hacia el objetivo</li>
+ *   <li>{@link #EXPERT}: siempre elige la dirección libre más cercana al objetivo</li>
+ * </ul>
+ *
+ * <p>Ambos perfiles detectan atasco (8 ticks sin moverse) y recalculan dirección.</p>
  */
 public enum MachineProfile {
 
